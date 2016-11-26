13369810f6b1bfa9a682724708ce2845a6265394
boamorte8 <salazar.z.esteban@gmail.com>
2016-11-26 15:51:48 -0500

    Update ports for start

--- server/server.js
+++ server/server.js
@@ -5,16 +5,19 @@ let app = express();
 
 let root = path.normalize(__dirname + '/..');
 
+app.set('port', (process.env.PORT || port));
+
 app.set('appPath', path.join(root, 'client'));
 app.use(express.static(app.get('appPath')));
 
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html'); 
+app.set('view engine', 'html');
 
 app.route('/*')
   .get((req, res) => {
     res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
   });
 
-app.listen(port);
-console.log('Listening on port ' + port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
